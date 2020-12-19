8bd00411524f48904e90228528dcbd5e55937ffb
John Juvi De Villa <drjjdevilla2002@gmail.com>
2020-12-20 02:07:20 +0800

    fixed sliding layout

--- android-test-app/obj/Debug/90/android/environment.arm64-v8a.s
+++ android-test-app/obj/Debug/90/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"19331564-11db-43d3-bd4c-ede29023733f"
+	.asciz	"ce3058d3-2dfa-41d2-9b17-38bf53212ea7"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
